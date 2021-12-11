1ddd360c1ab09e5473cd7b6f7dd3572366b0de38
Gabriel Moraes <gabriel.moraes@iclubs.com.br>
2021-12-10 23:23:24 -0400

    Tipos de dados: Objetos

--- www/curso-php/tipo-objetos.php
+++ www/curso-php/tipo-objetos.php
@@ -14,21 +14,59 @@
 
 		<h3>Propriedades</h3>
 
+		<?php 
+			class carro {
+				private $ano;
+				private $modelo;
+				private $placa;
+				private $cpf_dono;
+
+				function __construct($ano,$modelo,$placa,$cpf) {
+					$this->ano = $ano;
+					$this->modelo = $modelo;
+					$this->placa = $placa;
+					$this->cpf_dono = $cpf;
+				}
+
+				public function mostrar_info() {
+					echo 'Ano:' . $this->ano . '<br>'; 
+					echo 'MOdelo:' . $this->modelo . '<br>';
+					echo 'Placa:' . $this->placa . '<br>';
+				}
+
+				public function mostrar_cpf() {
+					echo $this->cpf_dono . '<br>'; 
+				}
+
+				public function mostrar_dono() {
+					$this->cpf_dono = $cpf; 
+				}
+
+				public function mudar_dono($cpf) {
+					$this->cpf_dono = $cpf; 
+				}
+			}
+
+			$carro1 = new carro(2018,'fox','aaa2020','111.222.333-45');
+		
+
+		?>
+
 		<h4>Propriedades do Objeto Carro1: </h4>
-		<pre></pre>
+		<pre><?php echo $carro1->mostrar_info(); ?></pre>
 
 		<h3>Métodos</h3>
 
 		<h4>Dono do Carro1: </h4>
-		<p></p>
+		<p><?php $carro1->mostrar_cpf(); ?></p>
 		<br>
 		
 		<h4>---------- Troca do dono do carro ----------</h4>
 
 		<br>
-
+		<?php $carro1->mudar_dono('200.300.400-55'); ?>
 		<h4>Novo Dono do Carro1: </h4>
-		<p></p>
+		<p><?php $carro1->mostrar_cpf(); ?></p>
 
 
 

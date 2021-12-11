994b2218ed33b28629f570de4662d2fa966dba0e
Gabriel Moraes <gabriel.moraes@iclubs.com.br>
2021-12-11 15:49:02 -0400

    Validação de Formulários (Parte 1)

--- www/curso-php/valida-formularios.php
+++ www/curso-php/valida-formularios.php
@@ -11,9 +11,45 @@
 		<hr>
 		<small>Curso de Básico de PHP - Prof. Ivan Lourenço Gomes</small>
 
+		<h3>Envie seus dados</h3>
+
+		<?php 
+			if($_SERVER['REQUEST_METHOD'] == 'POST') {
+				$nome = $_POST['nome'];
+				$email = $_POST['email'];
+
+				if($nome == '') {
+					$erro_nome = '* O nome é obrigatório';
+				} elseif($email == '') {
+					$erro_email = '* O email é obrigatório ';
+				}
+			}
+		?>
+
+		<form action="valida-formularios.php" method="post">
+			
+			Nome: *
+			<br>
+			<input type="text" name="nome" class="field">
+			<br>
+				<div class="erro-form"><?php echo $erro_nome ?></div>
+			<br>
+			
+			E-mail: *
+			<br>
+			<input type="text" name="email" class="field">
+			<br>
+				<div class="erro-form"><?php echo $erro_email ?></div>
+			<br>
+
+			<input type="submit" name="submit" class="submit"><br>
+			<br>
+				<div class="sucesso-form"></div>
+			<br>
+		</form>
 
 		<h3>Confirmaçao</h3>
-		<p>Obrigado!</p>
+		<pre><?php print_r($_POST); ?></pre>
 
 		
 

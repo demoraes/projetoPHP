cfbf238082f36d93ef479af32a16d264c3a6cd88
Gabriel Moraes <gabriel.moraes@iclubs.com.br>
2021-12-18 14:02:40 -0400

    Configuração do PHPMailer

--- www/restaurante/index.php
+++ www/restaurante/index.php
@@ -194,22 +194,87 @@
                             $telefone = $_POST['telefone'];
                             $data = $_POST['data'];
                             $num_pessoas = $_POST['num_pessoas'];
+
+                            $nome = clean_input($nome);
+                            $email = clean_input($email);
+                            $mensagem = clean_input($mensagem);
+                            $telefone = clean_input($telefone);
+                            $data = clean_input($data);
+                            $num_pessoas = clean_input($num_pessoas);
                         }
 
-                        $nome = clean_input($nome);
-                        $email = clean_input($email);
-                        $mensagem = clean_input($mensagem);
-                        $telefone = clean_input($telefone);
-                        $data = clean_input($data);
-                        $num_pessoas = clean_input($num_pessoas);
+                        // Inserir Arquivos do PHPMailer
+                        require 'phpmailer/Exception.php';
+                        require 'phpmailer/PHPMailer.php';
+                        require 'phpmailer/SMTP.php';
 
-                        echo $nome . '<br>';
-                        echo $email . '<br>';
-                        echo $mensagem . '<br>';
-                        echo $telefone . '<br>';
-                        echo $data . '<br>';
-                        echo $num_pessoas . '<br>';
+                        // Usar as classes sem o namespace
+                        use PHPMailer\PHPMailer\PHPMailer;
+                        use PHPMailer\PHPMailer\Exception;
 
+                        // Criação do Objeto da Classe PHPMailer
+                        $mail = new PHPMailer(true); 
+
+
+                        try {
+                            
+                            //Retire o comentário abaixo para soltar detalhes do envio 
+                            // $mail->SMTPDebug = 2;                                
+                            
+                            // Usar SMTP para o envio
+                            $mail->isSMTP();                                      
+
+                            // Detalhes do servidor (No nosso exemplo é o Google)
+                            $mail->Host = 'smtp.gmail.com';
+
+                            // Permitir autenticação SMTP
+                            $mail->SMTPAuth = true;                               
+
+                            // Nome do usuário
+                            $mail->Username = 'cbgabriel2016@gmail.com';        
+                            // Senha do E-mail         
+                            $mail->Password = 'vuj@d33xp1r10n';                           
+                            // Tipo de protocolo de segurança
+                            $mail->SMTPSecure = 'tls';   
+
+                            // Porta de conexão com o servidor                        
+                            $mail->Port = 587;
+
+                            
+                            // Garantir a autenticação com o Google
+                            $mail->SMTPOptions = array(
+                                'ssl' => array(
+                                    'verify_peer' => false,
+                                    'verify_peer_name' => false,
+                                    'allow_self_signed' => true
+                                )
+                            );
+
+                            // Remetente
+                            $mail->setFrom('from@example.com', 'Mailer');
+                            
+                            // Destinatário
+                            $mail->addAddress('cbgabriel2016@gmail.com', 'Joe User');
+
+                            // Conteúdo
+
+                            // Define conteúdo como HTML
+                            $mail->isHTML(true);                                  
+
+                            // Assunto
+                            $mail->Subject = 'Insira o assunto';
+                            $mail->Body    = 'Insira o texto do e-mail';
+                            $mail->AltBody = 'Formato alternativo em texto puro para emails que não aceitam HTML';
+
+                            // Enviar E-mail
+                            $mail->send();
+                            echo 'Mensagem enviada com sucesso';
+                        } catch (Exception $e) {
+                            echo 'A mensagem não foi enviada pelo seguinte motivo: ', $mail->ErrorInfo;
+                        }
+                                
+
+                
                     ?>
 
                 </div>
